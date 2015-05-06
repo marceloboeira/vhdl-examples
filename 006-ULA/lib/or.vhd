5d1fb98af460546c84177f1d29bb7c0b497ad84b
Marcelo Boeira <me@marceloboeira.com>
2015-05-06 20:52:57 -0300

    Update or.vhd

--- 006-ULA/lib/or.vhd
+++ 006-ULA/lib/or.vhd
@@ -2,13 +2,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity pOR is
-    Port ( A : in  STD_LOGIC_VECTOR (7 downto 0);
-           B : in  STD_LOGIC_VECTOR (7 downto 0);
-           O : out  STD_LOGIC_VECTOR (7 downto 0));
-end pOR;
+entity OR_PROCESS is
+    Port (A : in  STD_LOGIC_VECTOR (7 downto 0);
+          B : in  STD_LOGIC_VECTOR (7 downto 0);
+          O : out  STD_LOGIC_VECTOR (7 downto 0));
+end OR_PROCESS;
 
-architecture Behavioral of pOR is
+architecture Behavioral of OR_PROCESS is
 
 begin
 	O <= A or B;
