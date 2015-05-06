75169dd119456ff13a204aecb5eb73886d7518c2
Marcelo Boeira <me@marceloboeira.com>
2015-05-06 20:53:11 -0300

    Update and.vhd

--- 006-ULA/lib/and.vhd
+++ 006-ULA/lib/and.vhd
@@ -1,13 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity pAND is
+entity AND_PROCESS is
     Port ( A : in  STD_LOGIC_VECTOR (7 downto 0);
            B : in  STD_LOGIC_VECTOR (7 downto 0);
            O : out  STD_LOGIC_VECTOR (7 downto 0));
-end pAND;
+end AND_PROCESS;
 
-architecture Behavioral of pAND is
+architecture Behavioral of AND_PROCESS is
+
 begin
   O <= A and B;
 end Behavioral;
