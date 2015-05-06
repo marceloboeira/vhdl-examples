dbf3748d1281fd6a7b8819b909096d54f0647998
Marcelo Boeira <me@marceloboeira.com>
2015-05-06 20:52:43 -0300

    Update subtracter.vhd

--- 006-ULA/lib/subtracter.vhd
+++ 006-ULA/lib/subtracter.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 
-entity SUB is
+entity SUBTRACTER_PROCESS is
     Port ( A : in  STD_LOGIC_VECTOR (7 downto 0);
            B : in  STD_LOGIC_VECTOR (7 downto 0);
            O : out  STD_LOGIC_VECTOR (7 downto 0));
-end SUB;
+end SUBTRACTER_PROCESS;
 
-architecture Behavioral of SUB is
+architecture Behavioral of SUBTRACTER_PROCESS is
 
 begin
-	 O <= A - B;
+  O <= A - B;
 end Behavioral;
 
