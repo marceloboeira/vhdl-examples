67078dd6b83c74c355a77981c7dfa793c88cd803
Marcelo Boeira <me@marceloboeira.com>
2015-05-06 20:59:00 -0300

    Update ula_tb.vhd

--- 006-ULA/test/ula_tb.vhd
+++ 006-ULA/test/ula_tb.vhd
@@ -4,36 +4,37 @@ use ieee.std_logic_1164.all;
 entity ULA_tb is
 end ULA_tb;
  
+
 architecture behavior of ULA_tb is 
    component ULA
    port(A : in  std_logic_vector(7 downto 0);
         B : in  std_logic_vector(7 downto 0);
-        Sel : in std_logic_vector(1 downto 0);
+		  S : in std_logic_vector(1 downto 0);
         O : out  std_logic_vector(7 downto 0));
    end component;
     
    signal A : std_logic_vector(7 downto 0) := (others => '0');
    signal B : std_logic_vector(7 downto 0) := (others => '0');
-   signal Sel : std_logic_vector(1 downto 0) := (others => '0');
+   signal S : std_logic_vector(1 downto 0) := (others => '0');
  	signal O : std_logic_vector(7 downto 0);
    
 begin
  
    uut: ULA port map(A => A,
-							B => B,
-							Sel => Sel,
-							O => O );
+		     B => B,
+		     S => S,
+		     O => O );
 
 	control : process
 	begin
 		A <= 10;
 		B <= 20;
-		Sel <= 0;
+		S <= 0;
 		wait for 15 ns;
 		
 		A <= 170;
 		B <= 170;
-		Sel <= 0;
+		S <= 0;
 		wait for 15 ns;
 
 		wait;
