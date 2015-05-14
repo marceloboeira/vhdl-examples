5125a0063d761e4701254d6719046f470717b5f9
Marcelo Boeira <me@marceloboeira.com>
2015-05-13 21:33:44 -0300

    Create parity_process.vhd

---
+++ 007-parity-analyser/lib/parity_process.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+
+entity PARITY_PROCESS is
+Port (A : in STD_LOGIC;
+		B : in STD_LOGIC;
+		O : out STD_LOGIC);
+		 
+end PARITY_PROCESS;
+
+architecture Behavioral of PARITY_PROCESS is
+begin
+	O <= A xor B;
+end Behavioral;
